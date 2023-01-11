d4aca7ef077819ec6f660c70cd366e6f244e9625
pmw0310 <mw.park@retigrid.com>
2023-01-11 15:41:54 +0900

    인형 아이콘 색변경 버그 수정

--- src/components/dollIcon.tsx
+++ src/components/dollIcon.tsx
@@ -1,80 +1,77 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { styled } from '@mui/material/styles';
 import colorMix from '../utils/blendColors';
 import Image from 'react-image-webp';
 import { Doll, rarityColors } from '../data/dolls';
+import './dollIcon.css';
 
 interface StyledIconProps {
    size: number;
    color?: string;
-   disabled?: boolean;
 }
 
-const StyledIcon = styled('div')<StyledIconProps>(
-   ({ theme, size, color, disabled = false }) => {
-      const mainColor: string = color || theme.palette.info.main;
-      const sideSize = Math.round(size / 4);
-      const sideIconSize = Math.round(size / 4.5);
+const StyledIcon = styled('div')<StyledIconProps>(({ theme, size, color }) => {
+   const mainColor: string = color || theme.palette.info.main;
+   const sideSize = Math.round(size / 4);
+   const sideIconSize = Math.round(size / 4.5);
 
-      return {
-         position: 'relative',
-         width: size,
-         height: size,
-         margin: 4,
-         padding: 0,
-         '.doll-icon': disabled
-            ? {
-                 filter: 'grayscale(100%)',
-                 opacity: 0.5,
-                 borderRadius: 6,
-              }
-            : {
-                 filter: 'grayscale(0%)',
-                 opacity: 1,
-                 borderRadius: 6,
-              },
-         '.rarity-border': {
-            position: 'absolute',
-            left: 0,
-            top: 0,
-            width: size,
-            height: size,
-            boxSizing: 'border-box',
-            border: `${mainColor} solid 3px`,
-            borderRadius: 6,
-            filter: `grayscale(${disabled ? '75%' : '0%'})`,
-         },
-         '.rarity-background': {
-            position: 'absolute',
-            left: 0,
-            top: 0,
-            width: size,
-            height: size,
-            backgroundColor: colorMix(mainColor, '#333', 0.6),
-            borderRadius: 6,
-            filter: `grayscale(${disabled ? '75%' : '0%'})`,
-         },
-         '.side-background': {
-            position: 'absolute',
-            left: 0,
-            top: 0,
-            WebkitBorderTopLeftRadius: 6,
-            borderBottom: `${sideSize}px solid transparent`,
-            borderTop: `${sideSize}px solid #1c1b20`,
-            borderLeft: `${sideSize}px solid #1c1b20`,
-            borderRight: `${sideSize}px solid transparent`,
-         },
-         '.side-icons': {
-            position: 'absolute',
-            left: 2,
-            top: 2,
-            width: sideIconSize,
-            height: sideIconSize,
-            objectFit: 'contain',
-         },
-      };
-   }
-);
+   return {
+      // position: 'relative',
+      // width: size,
+      // height: size,
+      // margin: 4,
+      // padding: 0,
+
+      // '.doll-icon': {
+      //    filter: 'grayscale(0%)',
+      //    opacity: 1,
+      //    borderRadius: 6,
+      // },
+      // '.rarity-border': {
+      //    position: 'absolute',
+      //    left: 0,
+      //    top: 0,
+      //    width: size,
+      //    height: size,
+      //    boxSizing: 'border-box',
+      //    border: `${mainColor} solid 3px`,
+      //    borderRadius: 6,
+      //    filter: 'grayscale(0%)',
+      // },
+      // '.rarity-background': {
+      //    position: 'absolute',
+      //    left: 0,
+      //    top: 0,
+      //    width: size,
+      //    height: size,
+      //    backgroundColor: colorMix(mainColor, '#333', 0.6),
+      //    borderRadius: 6,
+      //    filter: 'grayscale(0%)',
+      // },
+      // '.side-background': {
+      //    position: 'absolute',
+      //    left: 0,
+      //    top: 0,
+      //    BorderTopLeftRadius: 6,
+      // },
+      // '.side-icons': {
+      //    position: 'absolute',
+      //    left: 2,
+      //    top: 2,
+      //    width: sideIconSize,
+      //    height: sideIconSize,
+      //    objectFit: 'contain',
+      // },
+
+      '&.image-disabled .doll-icon': {
+         filter: 'grayscale(100%)',
+         opacity: 0.5,
+      },
+      '&.image-disabled .rarity-border, &.image-disabled .rarity-background': {
+         filter: 'grayscale(75%)',
+      },
+   };
+});
 
 interface DollIconProps {
    size?: number;
@@ -86,32 +83,66 @@ const ImageIcon: React.FC<DollIconProps> = ({
    size = 48,
    doll,
    disabled = false,
-}) => (
-   <StyledIcon
-      size={size}
-      color={rarityColors[doll.rarity]}
-      disabled={disabled}
-   >
-      <div className="rarity-background" />
-      <Image
-         className="doll-icon"
-         src={doll.iconPng}
-         webp={doll.iconWebp}
-         width={size}
-         height={size}
-      />
-      <div className="rarity-border" />
-      {doll.sideIcon && (
-         <>
-            <div className="side-background" />
-            <Image
-               className="side-icons"
-               src={doll.sideIcon.iconPng}
-               webp={doll.sideIcon.iconWebp}
-            />
-         </>
-      )}
-   </StyledIcon>
-);
+}) => {
+   const mainColor = rarityColors[doll.rarity];
+   const sideSize = Math.round(size / 4);
+   const sideIconSize = Math.round(size / 4.5);
+
+   return (
+      <div
+         style={{
+            width: size,
+            height: size,
+         }}
+         className={`doll-icon-main ${
+            disabled ? ' doll-icon-disabled' : undefined
+         }`}
+      >
+         <div
+            style={{
+               width: size,
+               height: size,
+               backgroundColor: colorMix(mainColor, '#333', 0.6),
+            }}
+            className="doll-icon-rarity-background"
+         />
+         <Image
+            className="doll-icon-image"
+            src={doll.iconPng}
+            webp={doll.iconWebp}
+            width={size}
+            height={size}
+         />
+         <div
+            style={{
+               width: size,
+               height: size,
+               border: `${mainColor} solid 3px`,
+            }}
+            className="doll-icon-rarity-border"
+         />
+         {doll.sideIcon && (
+            <>
+               <div
+                  style={{
+                     borderBottom: `${sideSize}px solid transparent`,
+                     borderTop: `${sideSize}px solid #1c1b20`,
+                     borderLeft: `${sideSize}px solid #1c1b20`,
+                     borderRight: `${sideSize}px solid transparent`,
+                  }}
+                  className="doll-icon-side-background"
+               />
+               <Image
+                  className="doll-icon-side-icon"
+                  src={doll.sideIcon.iconPng}
+                  webp={doll.sideIcon.iconWebp}
+                  width={sideIconSize}
+                  height={sideIconSize}
+               />
+            </>
+         )}
+      </div>
+   );
+};
 
 export default React.memo(ImageIcon);
